1820a1bf3f20ca79075f271cb21966088254ff60
Jake Stone <Stonedev12@gmail.com>
2021-07-12 19:03:45 -0700

    Completed javascript-primitives-and-variables.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,20 @@
+var fullName = 'Jacob Stone';
+console.log('value of fullName: ' + fullName);
+console.log('typeof fullName: ' + typeof 'Jacob Stone');
+
+var isCool;
+isCool = true;
+console.log('value of isCool: ' + isCool);
+console.log('typeof isCool: ' + typeof true);
+
+var totalPets = 1;
+console.log('value of totalPets: ' + totalPets);
+console.log('type of totalPets: ' + typeof 1);
+
+var unknown = null;
+console.log('value of unknown: ' + unknown);
+console.log('type of unknown: ' + typeof unknown);
+
+var never;
+console.log('value of never: ' + never);
+console.log('typeof never: ' + typeof never);
