64b0ab667bd0a6c3eea6d6f59ccb0c5e9f31f1a3
Jake Stone <Stonedev12@gmail.com>
2021-07-29 19:11:22 -0700

    Complete dom-querying.

---
+++ dom-querying/main.js
@@ -0,0 +1,13 @@
+console.log('Hello, world');
+
+console.log(document.querySelector('h1'));
+console.dir(document.getElementsByTagName('h1'));
+
+console.log(document.querySelector('#explanation'));
+console.dir(document.getElementById('explanation'));
+
+console.log(document.querySelector('.hint'));
+console.dir(document.getElementsByClassName('hint'));
+
+console.log(document.querySelectorAll('p'));
+console.log(document.querySelectorAll('.example_link'));
