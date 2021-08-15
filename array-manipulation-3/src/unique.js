aef6101fa9a6638fc4a259a7655c663582474a65
Jake Stone <Stonedev12@gmail.com>
2021-08-14 18:30:29 -0700

    Complete array-manipulation.

---
+++ array-manipulation-3/src/unique.js
@@ -0,0 +1,10 @@
+/* exported unique */
+function unique(array) {
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (newArray.indexOf(array[i]) === -1) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
