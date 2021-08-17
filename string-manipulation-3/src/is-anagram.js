6a3263a0a5ee4a3cb0f577ea80e1add4cec1a1c1
Jake Stone <Stonedev12@gmail.com>
2021-08-16 18:10:00 -0700

    no verify string-manipulation-3.

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,1 +1,37 @@
 /* exported isAnagram */
+function isAnagram(firstString, secondString) {
+  var newStringOne = firstString.replace(/ /g, '');
+  var newStringTwo = secondString.replace(/ /g, '');
+
+  var noSpaceArray1 = newStringOne.split('');
+  var noSpaceArray2 = newStringTwo.split('');
+
+  console.log(noSpaceArray1);
+  console.log(noSpaceArray2);
+
+  if (noSpaceArray1.length !== noSpaceArray2.length) {
+    return false;
+  } else {
+    for (var i = 0; i < noSpaceArray1.length; i++) {
+      if (noSpaceArray2.indexOf(noSpaceArray1[i]) === -1) {
+        return false;
+      } else {
+        continue;
+      }
+    }
+    for (var j = 0; j < noSpaceArray1.length; j++) {
+      if (noSpaceArray1.indexOf(noSpaceArray2[j]) === -1) {
+        return false;
+      } else {
+        continue;
+      }
+    }
+    return true;
+  }
+}
+
+/* split the strings into two arrays of characters.
+loop through the first array and check if that character is in the other
+array. If not, return false.
+if character is a space continue.
+*/
